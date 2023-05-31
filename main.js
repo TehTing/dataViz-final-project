733744cb4c847b29508f1710299253fd45924fb3
Jacky Yang <yunchiehyang@gmail.com>
2023-05-31 13:22:15 +0800

    version0.5 cannot get list

--- main.js
+++ main.js
@@ -1,3 +1,11 @@
+//Data tools
+function formatTicks(d){
+    return d3.format('~s')(d)
+    .replace('M','mil')
+    .replace('G','bil')
+    .replace('T','tri')
+}
+
 const parseNA = string => (string === 'NA' ? undefined : string);
 function type(d) {
     return {
@@ -84,6 +92,62 @@ function classify(data, basis) {
     return dataArray;
 }
 
+function setupCanvas(barChartData){
+    const svg_width = 400;
+    const svg_height = 500;
+    const chart_margin = {top:80,right:40,bottom:40,left:80}; //留空間
+    const chart_width = svg_width - (chart_margin.left + chart_margin.right);
+    const chart_height = svg_height - (chart_margin.top + chart_margin.bottom);
+
+    const this_svg = d3.select('.bar-chart-container').append('svg')
+                    .attr('width',svg_width).attr('height',svg_height)
+                    .append('g')
+                    .attr('transform',`translate(${chart_margin.left},${chart_margin.top})`);
+    //Find min & max
+    const xExtent = d3.extent(barChartData,d=>d.pop);
+    //debugger;
+    const xScale_v1 = d3.scaleLinear().domain(xExtent).range([0, chart_width]);    
+    //only max
+    const xMax = d3.max(barChartData, d=>d.pop);
+    const xScale_v2 = d3.scaleLinear().domain([0,xMax]).range([0, chart_width]);
+    //簡潔一點
+    const xScale_v3 = d3.scaleLinear([0,xMax],[0, chart_width]);
+    
+    const yScale = d3.scaleBand().domain(barChartData.map(d=>d.top_genre))
+                                .rangeRound([0,chart_height])
+                                .paddingInner(0.25);
+    console.log(yScale.bandwidth());
+
+    //Draw bars
+    const bars = this_svg.selectAll('.bar')
+                         .data(barChartData)
+                         .enter()//把圖顯示出來
+                         .append('rect')
+                         .attr('class','bar')
+                         .attr('x',0)
+                         .attr('y',d=>yScale(d.top_genre))
+                         .attr('width',d=>xScale_v3(d.pop))
+                         .attr('height',yScale.bandwidth())
+                         .style('fill','dodgerblue'); 
+                         
+const header = this_svg.append('g').attr('class','bar-header')
+                        .attr('transform',`translate(0,${-chart_margin.top/2})`)
+                        .append('text');
+header.append('tspan').text('Total popularity by genre');
+header.append('tspan').text('Years:Since 2000')
+      .attr('x',0)
+      .attr('y',20).style('font-size','0.8em').style('fill','#555');
+
+const xAxis = d3.axisTop(xScale_v3)
+                .tickFormat(formatTicks)
+                .tickSizeInner(-chart_height)
+                .tickSizeOuter(0);
+const xAxisDraw = this_svg.append('g').attr('class','x axis').call(xAxis);
+
+const yAxis = d3.axisLeft(yScale).tickSize(0);
+const yAxisDraw = this_svg.append('g').attr('class','y axis').call(yAxis);
+yAxisDraw.selectAll('text').attr('dx','-0.6em');
+}
 
 
 function process(music) {
@@ -107,13 +171,12 @@ function process(music) {
             return d3.descending(a.pop, b.pop);
         }
     );
-
+    setupCanvas(dataClassified);
     console.log("raw data", music);
     console.log("filtered", dataClean);
-    console.log("classified", dataClassified)
+    console.log("classified", dataClassified);
     return dataClassified;
 }
-
 
 
 d3.csv('data/spotify.csv', type).then(
